cbcaeb9f66992b48f0ba9bd14ea1d8ff25adedfc
saeedjamali89@gmail.com <saeedjamali89@gmail.com>
2025-01-28 19:06:34 +0330

    initial swiper

--- src/app/_components/List/IdCard.js
+++ src/app/_components/List/IdCard.js
@@ -18,52 +18,59 @@ export default function IdCard({ id }) {
     // <div className="p-8 mb-1 relative border-l-8 transition-all bg-white border-slate-400 hover:border-cyan-400 [counter-increment:post-index] before:content-[counter(post-index)] before:p-2 before:leading-none before:absolute before:top-0 before:right-0 before:transition-all before:bg-slate-100 hover:before:bg-cyan-100 ">
 
     // </div>
-    <div
-      className={` w-full col-span-1 h-[218px]   shadow-2xl relative bg-glass`}
-      style={{ boxShadow: `2px 2px  5px ${id?.color}` }}
-    >
-      <div className=" w-full relative flex items-center justify-center text-[12px]">
-        <div className="w-20 h-20   flex items-center justify-center p-2">
-          <Image alt={""} width={100} height={100} src={id?.icon}></Image>
-        </div>
-        {/* <span className="absolute top-4 right-3  w-6 h-6 rounded-full ">
+    <div className="p-2">
+      <div
+        className={` w-full col-span-1 h-[180px]   shadow-2xl relative bg-glass bg-white`}
+        style={{ boxShadow: `2px 2px  5px ${id?.color}` }}
+      >
+        <div className=" w-full relative flex items-center justify-center text-[12px]">
+          <div className="w-16 h-16   flex items-center justify-center p-2">
+            <Image alt={""} width={100} height={100} src={id?.icon}></Image>
+          </div>
+          {/* <span className="absolute top-4 right-3  w-6 h-6 rounded-full ">
           <FaEye className="w-6 h-6"/>
         </span>
         <span className="absolute top-4 left-3  w-6 h-6 rounded-full ">
           <FaRegHeart className="w-6 h-6"/>
         </span> */}
-        {/* <span className="absolute bottom-4 left-3  w-6 h-6 rounded-full ">
+          {/* <span className="absolute bottom-4 left-3  w-6 h-6 rounded-full ">
           <RiDiscountPercentFill className="w-6 h-6"/>
         </span> */}
-      </div>
-      <div className="w-full flex flex-col items-center justify-center">
-        <h2 className="font-semibold text-header flex items-center justify-center mt-2 text-[12px] h-4 w-4">
-          {id?.latin}
-          <Image alt="1" width={20} height={20} src={id?.icon}></Image>
-        </h2>
-      </div>
-      <div className="flex items-center justify-center gap-2 mt-4 border-blue-100 font-iranyekanBold ">
-        سرگرمی
-      </div>
-      {isView && (
-        <div className="flex items-center justify-center gap-1 mt-4 text-[12px] ">
-          <p> {memberToK(150000)}</p>
-          <MdOutlineRemoveRedEye />
         </div>
-      )}
-      {isMember && (
-        <div className="flex items-center justify-center gap-1 mt-4 text-[12px] font-bold">
-          <p> {memberToK(150000)}</p>
-          <CiUser className="font-bold" />
+        <div className="w-full flex flex-col items-center justify-center">
+          <h2 className="font-semibold text-header flex items-center justify-center mt-2 text-[12px] h-4 w-4">
+            {id?.latin}
+            <Image alt="1" width={20} height={20} src={id?.icon}></Image>
+          </h2>
         </div>
-      )}
-      {isPrice && 
-        <div className="flex items-center justify-center gap-1 mt-4 text-[12px] font-bold">
-        <p> {(150000).toLocaleString()+ " تومان"} </p>
-        {/* <CiMoneyBill className="font-bold" /> */}
-      </div>}
-      {/* <div className="absolute bottom-0 left-0 right-0 flex items-center justify-center h-12 hover:bg-blue-500">
+        <div className="flex items-center justify-center gap-2 mt-2 font-iranyekanBold ">
+          سرگرمی
+        </div>
+        {isView && (
+          <div className="flex items-center justify-center gap-1 mt-2 text-[12px] ">
+            <p> {memberToK(150000)}</p>
+            <MdOutlineRemoveRedEye />
+          </div>
+        )}
+        {isMember && (
+          <div className="flex items-center justify-center gap-1 mt-2 text-[12px] font-bold">
+            <p> {memberToK(150000)}</p>
+            <CiUser className="font-bold" />
+          </div>
+        )}
+        {isPrice && (
+          <div className="flex items-center justify-center gap-1 mt-2 text-[12px] font-bold">
+            <p> {(150000).toLocaleString() + " تومان"} </p>
+            {/* <CiMoneyBill className="font-bold" /> */}
+          </div>
+        )}
+        {/* <div className="absolute bottom-0 left-0 right-0 flex items-center justify-center h-12 hover:bg-blue-500">
         ارتباط با دارنده
+      </div> */}
+      </div>
+      {/* <div className="w-full flex items-center justify-between gap-1  text-[12px] p-2">
+        <p> {(150000).toLocaleString() + " تومان"} </p>
+        <p> {(150000).toLocaleString() + " تومان"} </p>
       </div> */}
     </div>
   );
--- src/app/_components/swiper/SwiperTop10.js
+++ src/app/_components/swiper/SwiperTop10.js
@@ -14,16 +14,32 @@ import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import IdCard from "../List/IdCard";
 import { messengers } from "@/config/constants";
 import { Button } from "@nextui-org/react";
+import Image from "next/image";
 
 export default function SwiperTop10() {
   const [swiperRef, setSwiperRef] = useState(null);
 
   return (
     <div className="bg-header p-4 rounded-lg">
-      <div className="flex flex-grow gap-2 ">
-        <Button className="bg-header text-white hover:text-btn-orange cursor-pointer">بیشترین اعضا</Button>
-        <Button className="bg-header text-white hover:text-btn-orange cursor-pointer">بیشترین مشاهده</Button>
-        <Button className="bg-header text-white hover:text-btn-orange cursor-pointer">بیشترین قیمت</Button>
+      <div className="flex justify-between items-center ">
+        <div className="flex flex-grow gap-1 md:gap-2 px-2 ">
+          <span className="hover:bg-slate-100 hover:text-orange-700 text-white bg-btn-orange cursor-pointer p-2  text-[12px] rounded-lg">
+            عضو
+          </span>
+          <span className="hover:bg-slate-100 hover:text-orange-700  text-white bg-btn-orange cursor-pointer p-2  text-[12px] rounded-lg">
+            مشاهده
+          </span>
+          <span className="hover:bg-slate-100 hover:text-orange-700 text-white bg-btn-orange cursor-pointer p-2  text-[12px] rounded-lg">
+            قیمت
+          </span>
+        </div>
+        <Image
+          src={"/images/top10.png"}
+          width={100}
+          height={100}
+          alt="Top10"
+          className="w-8 h-8"
+        />
       </div>
       <Swiper
         loop
@@ -44,27 +60,35 @@ export default function SwiperTop10() {
         breakpoints={{
           0: {
             slidesPerView: 1,
-            spaceBetween: 10,
+            spaceBetween: 5,
           },
-          640: {
+          320: {
             slidesPerView: 2,
-            spaceBetween: 20,
+            spaceBetween: 0,
+          },
+          540: {
+            slidesPerView: 3,
+            spaceBetween: 0,
           },
           768: {
             slidesPerView: 4,
-            spaceBetween: 40,
+            spaceBetween: 5,
           },
           1280: {
             slidesPerView: 5,
-            spaceBetween: 50,
+            spaceBetween: 10,
+          },
+          1480: {
+            slidesPerView: 6,
+            spaceBetween: 10,
           },
         }}
-        navigation={true}
+        // navigation={true}
         modules={[Autoplay, Navigation]}
       >
         {messengers.map((item) => {
           return (
-            <SwiperSlide className="rounded-lg ">
+            <SwiperSlide className="rounded-lg  bg-transparent">
               <IdCard id={item} />
             </SwiperSlide>
           );
