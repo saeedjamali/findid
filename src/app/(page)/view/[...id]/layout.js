ac65d392ca5e6c5aa8adcb18b90379465bdfd035
saeedjamali89@gmail.com <saeedjamali89@gmail.com>
2025-02-13 19:48:59 +0330

    modify open graph

--- src/app/(page)/view/[...id]/layout.js
+++ src/app/(page)/view/[...id]/layout.js
@@ -3,33 +3,17 @@ import AppProvider from "@/app/context/AppProvider";
 import { Id_PER_PAGE } from "@/config/constants";
 import { authenticateUser } from "@/utils/authenticateMe";
 import { addSiteJsonLd } from "@/utils/schemasSeo";
-import { NextSeo } from "next-seo";
 
 export const metadata = {
   title: "مشاهده آگهی",
   description:
-    "بزرگترین بستر تبادل آیدی و صفحات اینترنتی- خرید و فروش کانال،پیج،گروه و آیدی های پیامرسان های مختلف",
+     "بزرگترین بستر تبادل آیدی و صفحات اینترنتی- خرید و فروش کانال،پیج،گروه و آیدی های پیامرسان های مختلف"
 };
-export default async function RootLayout({ children, searchParams }) {
+export default async function RootLayout({ children }) {
   const isAuthenticateUser = await authenticateUser();
-
   // const initialIds = await getIds(0, Id_PER_PAGE);
   return (
     <>
-      <NextSeo
-        title={"ads?.title"}
-        description={"ads?.description"}
-        openGraph={{
-          title: "openGraph - ads?.title",
-          description: "openGraph - ads?.description",
-          url: ` ${GLOBAL_URL}/view/${"ads?.title"}?id=${"ads?._id"}`,
-          images: [
-            {
-              url: `https://findid.ir/api/ads/image/173930059008288613profile%20(5).jpg/profile`,
-            },
-          ],
-        }}
-      />
       <AppProvider
         isAuthenticateUser={JSON.parse(JSON.stringify(isAuthenticateUser))}
       >
