e38c782411df2dae637e062746ecd24f0aa1b3e1
saeedjamali89@gmail.com <saeedjamali89@gmail.com>
2025-02-13 19:14:57 +0330

    modify open graph

--- src/app/(page)/view/[...id]/layout.js
+++ src/app/(page)/view/[...id]/layout.js
@@ -3,6 +3,7 @@ import AppProvider from "@/app/context/AppProvider";
 import { Id_PER_PAGE } from "@/config/constants";
 import { authenticateUser } from "@/utils/authenticateMe";
 import { addSiteJsonLd } from "@/utils/schemasSeo";
+import { NextSeo } from "next-seo";
 
 export const metadata = {
   title: "مشاهده آگهی",
@@ -12,10 +13,23 @@ export const metadata = {
 export default async function RootLayout({ children, searchParams }) {
   const isAuthenticateUser = await authenticateUser();
 
-
   // const initialIds = await getIds(0, Id_PER_PAGE);
   return (
     <>
+      <NextSeo
+        title={"ads?.title"}
+        description={"ads?.description"}
+        openGraph={{
+          title: "openGraph - ads?.title",
+          description: "openGraph - ads?.description",
+          url: ` ${GLOBAL_URL}/view/${"ads?.title"}?id=${"ads?._id"}`,
+          images: [
+            {
+              url: `https://findid.ir/api/ads/image/173930059008288613profile%20(5).jpg/profile`,
+            },
+          ],
+        }}
+      />
       <AppProvider
         isAuthenticateUser={JSON.parse(JSON.stringify(isAuthenticateUser))}
       >
